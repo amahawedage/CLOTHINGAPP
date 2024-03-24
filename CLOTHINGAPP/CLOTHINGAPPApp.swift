007fa25682f0bd378d56f5d6542fff2d4b8219c4
amahawedage <89461013+amahawedage@users.noreply.github.com>
2024-03-24 10:28:44 -0700

    initial commit

---
+++ CLOTHINGAPP/CLOTHINGAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLOTHINGAPPApp.swift
+//  CLOTHINGAPP
+//
+//  Created by Anusha on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CLOTHINGAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
